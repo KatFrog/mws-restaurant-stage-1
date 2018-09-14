c3a3b2e84a546db54409f9081e602a02f8aacdeb
Kathryn Patterson <katp@wegrok.net>
2018-09-14 12:39:55 -0400

    Finished second version of the app

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -69,9 +69,11 @@ fetchRestaurantFromURL = (callback) => {
 fillRestaurantHTML = (restaurant = self.restaurant) => {
     const name = document.getElementById('restaurant-name');
     name.innerHTML = restaurant.name;
+    name.tabIndex = 0;
 
     const address = document.getElementById('restaurant-address');
     address.innerHTML = restaurant.address;
+    address.tabIndex = 0;
 
     const image = document.getElementById('restaurant-img');
     image.className = 'restaurant-img';
@@ -80,6 +82,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
     const cuisine = document.getElementById('restaurant-cuisine');
     cuisine.innerHTML = restaurant.cuisine_type;
+    cuisine.tabIndex = 0;
 
     // fill operating hours
     if (restaurant.operating_hours) {
@@ -99,10 +102,12 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
 
         const day = document.createElement('td');
         day.innerHTML = key;
+        day.tabIndex = 0;
         row.appendChild(day);
 
         const time = document.createElement('td');
         time.innerHTML = operatingHours[key];
+        time.tabIndex = 0;
         row.appendChild(time);
 
         hours.appendChild(row);
@@ -114,9 +119,10 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
     const container = document.getElementById('reviews-container');
-    const title = document.createElement('h2');
-    title.innerHTML = 'Reviews';
-    container.appendChild(title);
+    // const title = document.createElement('h2');
+    // title.innerHTML = 'Reviews';
+    // title.tabIndex = 0;+
+    // container.appendChild(title);
 
     if (!reviews) {
         const noReviews = document.createElement('p');
@@ -136,6 +142,8 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
     const li = document.createElement('li');
+    li.tabIndex = 0;
+    li.role = "listitem";
     const name = document.createElement('p');
     name.innerHTML = review.name;
     li.appendChild(name);
